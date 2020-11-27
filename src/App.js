0e11e5aff4edd02ce725729ba75039a898bcde3c
le000190 <le000190@algonquinlive.com>
2020-11-26 23:48:01 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, Card, Row, Col, CardTitle} from 'react-materialize';
-import 'materialize-css';
+import logo from './logo.svg'
+import './App.css'
+import { Button, Card, Row, Col, CardTitle } from 'react-materialize'
+import 'materialize-css'
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Details from './Details'
 
-function App() {
-
+function App () {
   return (
-    <div className="App">
-     <nav><div>First Page</div><div>Second Page</div></nav>
+    <Router>
+      <div className='App'>
+        <nav>
+          <NavLink className='navLink' to='/'>
+            Home
+          </NavLink>
+          <NavLink className='navLink' to='/details'>
+            Details
+          </NavLink>
+        </nav>
 
-     <h1>First Page</h1>
-     <img src="./leaf.png" alt="Leaf" id="mainImage"/>
+        <Switch>
+          <Route exact path='/'>
+            <h1>First Page</h1>
+            <img src='./leaf.png' alt='Leaf' id='mainImage' />
+          </Route>
 
-   
-    </div>
-   
-  );
+          <Route exact path='/details'>
+            <Details />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
-
+export default App
