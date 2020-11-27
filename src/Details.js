21b012e2c52a3d1b3d2c0420f25d0fea388ff66f
le000190 <le000190@algonquinlive.com>
2020-11-26 23:39:16 -0500

    added details component

---
+++ src/Details.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+import { Button, Card, Row, Col, CardTitle } from 'react-materialize'
+import 'materialize-css'
+
+function Details () {
+  return (
+    <div className='details'>
+      <p>Details Page</p>
+    </div>
+  )
+}
+
+export default Details
